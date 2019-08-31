cc61353dd4c132c185b88bcee50b28010100ca31
cumul <gg6123@naver.com>
2019-08-31 09:52:31 +0900

    표준적인 TypeScript 개발/빌드 환경 구축 (#2)
    * .gitignore 설정 개선
    
    * 빌드 디렉토리 추가
    
    * 의존성 관리를 위한 package.json 추가
    
    * Secret.RebootSequence가 없으면 건너뛰도록 예외 처리
    
    * README 추가
    
    * 빌드 설정 개선
    
    * node_modules/ 삭제
    
    * npm-scripts의 오류 정정
    
    * 변경된 경로에 맞추어서 nodemon.json 파일 수정
    
    * 변경된 경로에 맞추어서 FileHandler 수정
    
    * 코드 정리
    
    * 작성자 정보 변경

---
+++ README.md
@@ -0,0 +1,23 @@
+# KappaBot
+
+## 설치
+
+```
+git clone git@github.com:kirnot92/KappaBot.git
+npm install
+```
+
+## 실행
+
+### 개발용
+
+```
+npm start
+```
+
+### 배포용
+
+```
+npm run build
+node dist/main.js
+```
---
+++ src/scripts/scheduler.ts
@@ -0,0 +1,113 @@
+import { RecurrenceRule, Job, scheduleJob } from "node-schedule";
+import File from "./file";
+import Dictionary from "./dictionary";
+import CommandHandler from "./handler"
+import {Client, Channel, MessageOptions, TextChannel } from 'discord.js'
+
+export default class ScheduleHandler
+{
+    commandHandler: CommandHandler
+    onRunning: Dictionary<string, Job> = new Dictionary<string, Job>()
+    client: Client
+
+    constructor(fileHandler: CommandHandler, client: Client)
+    {
+        this.commandHandler = fileHandler;
+        this.client = client;
+    }
+
+    public Parse(day: string): number
+    {
+        switch(day)
+        {
+            case "월요일": return 1;
+            case "화요일": return 2;
+            case "수요일": return 3;
+            case "목요일": return 4;
+            case "금요일": return 5;
+            case "토요일": return 6;
+            case "일요일": return 7;
+            case "월": return 1;
+            case "화": return 2;
+            case "수": return 3;
+            case "목": return 4;
+            case "금": return 5;
+            case "토": return 6;
+            case "일": return 7;
+        }
+    }
+
+    public async LoadSchedule()
+    {
+        var fileNames = await File.ReadDir("./schedule/")
+
+        for (var i=0; i < fileNames.length; ++i)
+        {
+            var fileName = fileNames[i].replace('.txt', '');
+            var identifier: any, day, hour, others = fileName.split('.');
+            var channelId = identifier;
+            var command = await File.ReadFile('./schedule/' +fileName, "utf8");
+
+            var jobCreater = await this.GetJobCreater(identifier, day, hour, command);
+            jobCreater.Create(async () =>
+            {
+                var msg = await this.commandHandler.Handle(["$" + command], identifier)
+                var channel = this.client.channels.get(channelId);
+                var textChannel = (channel as TextChannel)
+                textChannel.send(msg)
+            })
+        }
+    }
+
+    public GetRule(day: string, hour: string): RecurrenceRule
+    {
+        var rule = new RecurrenceRule();
+        rule.dayOfWeek =  this.Parse(day);
+        rule.hour = parseInt(hour);
+        rule.minute = 0
+        return rule
+    }
+
+    public async AddJob(identifier: string, day: string, hour: string, command: string): Promise<JobCreater>
+    {
+        var path = "./schedule/" + identifier + "." + day + "." + hour
+        await File.Write(path, command)
+
+        return await this.GetJobCreater(identifier, day, hour, command);
+    }
+
+    public async GetJobCreater(identifier: string, day: string, hour: string, command: string): Promise<JobCreater>
+    {
+        var path = "./schedule/" + identifier + "." + day + "." + hour
+
+        if (this.onRunning.ContainsKey(path))
+        {
+            var scheduleJob = this.onRunning.MustGet(path)
+            scheduleJob.cancel()
+        }
+
+        var rule = this.GetRule(day, hour)
+        return new JobCreater(rule, (job:Job)=>
+        {
+            this.onRunning.Add(path, job);
+        })
+    }
+}
+
+export class JobCreater
+{
+    rule: RecurrenceRule
+    onCreate: (job: Job) => void
+
+    constructor(rule: RecurrenceRule, onCreate: (job:Job) => void)
+    {
+        this.rule = rule;
+        this.onCreate = onCreate;
+    }
+
+    public Create(func: Function)
+    {
+        var job = scheduleJob(this.rule, () => func());
+        this.onCreate(job);
+    }
+}
