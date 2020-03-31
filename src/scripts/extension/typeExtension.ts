ab6ec4c9be364929edec33b25068ba62daa79939
Beretta <kirnot92@gmail.com>
2020-03-31 12:22:25 +0900

    AnyChannel -> Channel 옮기기 위한 중간상태

--- src/scripts/extension/typeExtension.ts
+++ src/scripts/extension/typeExtension.ts
@@ -1,3 +1,4 @@
 import {TextChannel, DMChannel, GroupDMChannel } from "discord.js"
 
 export type AnyChannel = TextChannel | DMChannel | GroupDMChannel;
+export type Channel = TextChannel | DMChannel | GroupDMChannel;
