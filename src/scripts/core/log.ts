ec04af1055a633c7bf228dc89dc82e9907cb06ac
Beretta <kirnot92@gmail.com>
2020-04-06 12:23:25 +0900

    로거 러프

--- src/scripts/core/log.ts
+++ src/scripts/core/log.ts
@@ -8,12 +8,38 @@ export default class Log
 
     public static Info(message: string)
     {
+        message = this.AttachTimestamp(message);
+
         console.log(message);
     }
 
     public static Critical(message: string)
     {
+        message = this.AttachTimestamp(message);
+
         // 이건 관리자한테 바로 알려주기
         console.log(message);
+    }
+
+    private static AttachTimestamp(msg: string): string
+    {
+        return "[" + this.GetHHMMSS() + "] " + msg;
+    }
+
+    private static GetYYYMMDD(): string
+    {
+        var now = new Date();
+        return (now.getFullYear() + now.getMonth() + now.getDay()).toString();
+    }
+
+    private static GetHHMMSS(): string
+    {
+        var now = new Date();
+        var arr = new Array<string>();
+        arr.push(now.getHours().toString());
+        arr.push(now.getMinutes().toString());
+        arr.push(now.getSeconds().toString());
+
+        return arr.join(":");
     }
 }
