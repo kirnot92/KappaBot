9ff684ad45148aac4c06f3914d14c11e50b013e5
Beretta <kirnot92@gmail.com>
2020-05-09 00:13:17 +0900

    트위터 앱 메인에 적용 겸 엔진/메인 통합

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,35 @@
-import { KappaEngine } from "./scripts/core/kappaEngine";
+import Global from "./scripts/core/global";
+import Application from "./scripts/core/application";
+import Log from "./scripts/core/log";
+import {Client} from "discord.js";
+import * as Secret from "./json/secret.json";
+import * as SystemMessage from "./json/systemMessage.json";
+import TwitterApplication from "./scripts/core/twitterApp";
+
+class KappaEngine
+{
+    private static application: Application;
+    private static twitterApplication: TwitterApplication;
+
+    public static async Main()
+    {
+        Log.Initialize();
+
+        var client = new Client();
+        await client.login(Secret.Token);
+        Log.Info("Client Logged In");
+
+        Global.Initialize(client);
+        Log.Info("GlobalAPI Initialized");
+
+        await Global.Client.SendMessage(Secret.DefaultChannelId, SystemMessage.RebootCompleted);
+
+        this.application = new Application();
+        this.application.Initialize();
+
+        this.twitterApplication = new TwitterApplication();
+        this.twitterApplication.Initialize();
+    }
+}
 
 KappaEngine.Main();
