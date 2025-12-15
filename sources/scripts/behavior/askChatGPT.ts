0e045c802a18bdf1e9502b4d2182f770b94a8c32
Beretta <kirnot92@gmail.com>
2025-12-15 23:31:36 +0900

    Memory를 md화 해서 불필요한 정보 거르고 챗지피티에 삽입

--- sources/scripts/behavior/askChatGPT.ts
+++ sources/scripts/behavior/askChatGPT.ts
@@ -2,7 +2,7 @@ import CommandRepository from "../procedure/commandRepository";
 import CommandContext from "./commandContext";
 import Global from "../core/global";
 import { IBehavior } from "./IBehavior";
-import { MessageContext } from "../type/types";
+import { MessageContext, UserMemory, UserMemoryItem } from "../type/types";
 import { PartialUser, User } from "discord.js";
 
 export class AskChatGPT implements IBehavior
@@ -23,20 +23,21 @@ export class AskChatGPT implements IBehavior
     public async Run()
     {
         const fileName = `user_${this.author.id}.memory.json`;
-        let memoryData = "";
+        let memoryDataStr = "";
         if (await CommandRepository.IsExists(this.channelId, fileName))
         {
-            memoryData = (await CommandRepository.Load(this.channelId, fileName)).Message;
+            memoryDataStr = (await CommandRepository.Load(this.channelId, fileName)).Message;
         }
 
         const msgs = await Global.Client.SendMessage(this.channelId, "잠시만 기다려주세요! 생각 중이에요!");
 
         let instructions = (await this.GetPrompt()).Message;
         instructions += "input은 최근 대화를 포함하고 있습니다. 마지막 input이 사용자의 질문이며, 나머지는 이전 기록임을 참고하여 답변을 작성하세요.\n";
-        if (memoryData != "")
+        if (memoryDataStr != "")
         {
             instructions += "다음은 사용자에 대한 메모리 데이터입니다. 이를 활용하여 답변을 작성하세요.";
-            instructions += memoryData;
+            const memoryData =  JSON.parse(memoryDataStr) as UserMemory;
+            instructions += this.ConvertUserMemoryToMD(memoryData);
         }
 
         const response = await Global.ChatGPT.Request(instructions, this.messageHistory);
@@ -61,5 +62,30 @@ export class AskChatGPT implements IBehavior
         }
 
         return new CommandContext("");
+    }
+
+    private ConvertUserMemoryToMD(data: UserMemory): string
+    {
+        let r = "";
+        r += `\n# Memory (updated: ${data.updated_at})\n`
+        r += this.ConvertSectionToMD("Facts", data.items.facts);
+        r += this.ConvertSectionToMD("Constraints", data.items.constraints);
+        r += this.ConvertSectionToMD("Preferences", data.items.preferences);
+        r += this.ConvertSectionToMD("Projects", data.items.projects);
+        return r;
+    }
+
+    private ConvertSectionToMD(sectionName: string, section: UserMemoryItem[]): string
+    {
+        let r = "";
+        if (section.length > 0)
+        {
+            r += `\n## ${sectionName}\n`;
+            for (const item of section)
+            {
+                r += `- [${item.confidence} Confidence] ${item.text}\n`;
+            }
+        }
+        return r;
     }
 }
